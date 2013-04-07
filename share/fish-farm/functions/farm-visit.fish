158d2c9b541a43a7c9bc7857634c2143d8414f90
Terje Larsen <terlar@gmail.com>
2013-04-07 18:48:07 +0200

    Transfer environment

--- share/fish-farm/functions/farm-visit.fish
+++ share/fish-farm/functions/farm-visit.fish
@@ -1,13 +1,18 @@
 function farm-visit --description 'Visit project'
   set -l project $argv[1]
-  set -l command " cd $farm/$project; clear"
+  set -l commands " cd $farm/$project; clear"
 
   if not which tmux >/dev/null
-    eval $command
+    eval $commands
     return
   end
 
   set -l sessions (tmux list-sessions | cut -d ':' -f 1 ^/dev/null)
+  set -l environment (tmux show-environment | grep -v '^-' | sed 's|=| |')
+
+  for i in $environment
+    set commands " set -xg $i;$commands"
+  end
 
   # Wrapper command for OSX clipboard
   set -l wrapper
@@ -18,14 +23,14 @@ function farm-visit --description 'Visit project'
   if test -z "$TMUX"
     if not contains $project $sessions
       tmux new -d -s $project $wrapper
-      tmux send-keys -t $project $command Enter
+      tmux send-keys -t $project $commands Enter
     end
 
     tmux attach -t $project
   else
     if not contains $project $sessions
       tmux if true "new -d -s $project"
-      tmux send-keys -t $project $command Enter
+      tmux send-keys -t $project $commands Enter
     end
 
     tmux switch -t $project
