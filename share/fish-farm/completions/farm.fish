1f3423e848d365ff65926e8dbdc1fc472ae4b946
Terje Larsen <terlar@gmail.com>
2016-04-13 17:23:59 +0200

    completions: move logic to completion file

--- share/fish-farm/completions/farm.fish
+++ share/fish-farm/completions/farm.fish
@@ -1,1 +1,27 @@
-__farm_complete farm
+function __farm_no_command
+    set -l cmd (commandline -poc)
+    set -e cmd[1]
+    if test (count $cmd) -eq 0
+        return 0
+    end
+    return 1
+end
+
+function __farm_using_command
+    set cmd (commandline -poc)
+    if test (count $cmd) -gt 1
+        if test $argv[1] = $cmd[2]
+            return 0
+        end
+    end
+    return 1
+end
+
+function __farm_complete_commands
+	farm help | awk '/Commands:/{f=1;next}/^\s*$/{f=0}f' | string replace  -r '^\s+([^\s]+)\s+(.+)$' '$1'\t'$2'
+end
+
+complete -c farm -f -n '__farm_no_command' -a '(__farm_complete_commands)'
+complete -c farm -f -n '__farm_no_command' -a '(farm ls)' -d 'Project'
+complete -c farm -f -n '__farm_using_command cd'    -a '(farm ls)' -d 'Project'
+complete -c farm -f -n '__farm_using_command visit' -a '(farm ls)' -d 'Project'
